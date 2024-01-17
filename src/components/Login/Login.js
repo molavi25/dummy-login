b2011b582021b3104881da64ea442a7a01203bf9
Ali Molavi <alimolavi025@gmail.com>
2024-01-17 11:42:04 +0330

    .

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,78 +1,195 @@
-import React, { useState } from 'react';
+import React, {
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 
+import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
+  const [formIsValid, setFormIsValid] =
+    useState();
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  // useReducer
+  const [email, dispatchEmail] =
+    useReducer(
+      (state, action) => {
+        const actionType = action.type;
+        const actionPayload =
+          action.payLoad;
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
+        if (
+          actionType === 'USER_INPUT'
+        ) {
+          return {
+            value: actionPayload,
+            isValid:
+              actionPayload.includes(
+                '@',
+              ),
+          };
+        }
+
+        if (
+          actionType === 'INPUT_BLUR'
+        ) {
+          return {
+            value: state.value,
+            isValid:
+              state.value.includes('@'),
+          };
+        }
+
+        return {
+          value: '',
+          isValid: false,
+        };
+      },
+      { value: '', isValid: null },
     );
+  const [password, dispatchPassword] =
+    useReducer(
+      (state, action) => {
+        if (
+          action.type === 'USER_INPUT'
+        ) {
+          return {
+            value: action.payLoad,
+            isValid:
+              action.payLoad.trim()
+                .length > 6,
+          };
+        }
+
+        if (
+          action.type === 'INPUT_BLUR'
+        ) {
+          return {
+            value: state.value,
+            isValid:
+              state.value.trim()
+                .length > 6,
+          };
+        }
+
+        return {
+          value: '',
+          isValid: null,
+        };
+      },
+      { value: '', isValid: null },
+    );
+
+  // Debouncing
+  useEffect(() => {
+    const validator = setTimeout(() => {
+      setFormIsValid(
+        email.isValid &&
+          password.isValid,
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(validator);
+    };
+  }, [email.isValid, password.isValid]);
+
+  const emailChangeHandler = (
+    event,
+  ) => {
+    dispatchEmail({
+      type: 'USER_INPUT',
+      payLoad: event.target.value,
+    });
   };
 
-  const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+  const passwordChangeHandler = (
+    event,
+  ) => {
+    dispatchPassword({
+      type: 'USER_INPUT',
+      payLoad: event.target.value,
+    });
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({
+      type: 'INPUT_BLUR',
+    });
   };
 
-  const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
+  const validatePasswordHandler =
+    () => {
+      dispatchPassword({
+        type: 'INPUT_BLUR',
+      });
+    };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(
+      email.value,
+      password.value,
+    );
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
+          className={`${
+            classes.control
+          } ${
+            email.isValid === false
+              ? classes.invalid
+              : ''
+          }`}>
+          <label htmlFor='email'>
+            E-Mail
+          </label>
           <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
+            type='email'
+            id='email'
+            value={email.value}
+            onChange={
+              emailChangeHandler
+            }
+            onBlur={
+              validateEmailHandler
+            }
           />
         </div>
         <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
+          className={`${
+            classes.control
+          } ${
+            password.isValid === false
+              ? classes.invalid
+              : ''
+          }`}>
+          <label htmlFor='password'>
+            Password
+          </label>
           <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
+            type='password'
+            id='password'
+            value={password.value}
+            onChange={
+              passwordChangeHandler
+            }
+            onBlur={
+              validatePasswordHandler
+            }
           />
         </div>
-        <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+        <div
+          className={classes.actions}>
+          <Button
+            type='submit'
+            className={classes.btn}
+            disabled={!formIsValid}>
             Login
           </Button>
         </div>
